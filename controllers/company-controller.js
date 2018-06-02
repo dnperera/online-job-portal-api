93553044e8a01a7f24c403a80b01bed2e7dc3550
Dayan Perera <dayannp@gmail.com>
2018-06-02 07:51:58 -0700

    add get,post,delete routes to the company model

--- controllers/company-controller.js
+++ controllers/company-controller.js
@@ -1,6 +1,50 @@
 module.exports = {
 	async create(ctx) {
 		try {
+			//create new record
+			ctx.body = await ctx.db.Company.create({
+				name: ctx.request.body.name,
+				city: ctx.request.body.city,
+				address: ctx.request.body.address,
+			});
+		} catch (err) {
+			ctx.throw(500, err);
+		}
+	},
+
+	async find(ctx) {
+		try {
+			ctx.body = await ctx.db.Company.findAll({});
+		} catch (err) {
+			ctx.thow(500, err);
+		}
+	},
+	async findOne(ctx) {
+		try {
+			const company = await ctx.db.Company.findOne({
+				where: { id: ctx.params.id },
+			});
+			if (!company) {
+				ctx.throw(404, 'Company is not found.');
+			}
+			ctx.body = company;
+		} catch (err) {
+			ctx.throw(500, err);
+		}
+	},
+
+	async delete(ctx) {
+		try {
+			const results = await ctx.db.Company.destroy({
+				where: {
+					id: ctx.params.id,
+				},
+			});
+
+			results === 0
+				? ctx.throw(404, 'Company not found !!')
+				: `Compamy is deleted with id ${ctx.params.id}`;
+			ctx.body = results;
 		} catch (err) {
 			ctx.throw(500, err);
 		}
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = new Router();
 const { CompanyController } = require('../controllers');
 
 router.post('/companies', CompanyController.create);
-
+router.get('/companies', CompanyController.find);
+router.get('/companies/:id', CompanyController.findOne);
+router.delete('/companies/:id', CompanyController.delete);
 module.exports = router;
